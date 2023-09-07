8fcf15d7e47676f9db5bb306e9e578658db9b25d
Jean de Dieu BANCOLE <bancolejeandedieu@gmail.com>
2023-09-07 08:28:13 -0700

    advanced tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-	char *lachaine = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *chai = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	int i = 0;
 
-	while (lachaine[i] != '\0')
+	while (chai[i] != '\0')
 	{
-		write(1, &lachaine[i], 1);
+		write(1, &chai[i], 1);
 		i++;
 	}
 	return (1);
