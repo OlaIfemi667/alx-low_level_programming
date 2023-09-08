3c467752e3984ff404f812034f28ab1a9475ac9b
Jean de Dieu BANCOLE <bancolejeandedieu@gmail.com>
2023-09-08 09:17:07 -0700

    two digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers[10], i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		numbers[i] = i;
+	}
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i+1; j < 10; j++)
+		{
+			putchar('0' + numbers[i]);
+			putchar('0' + numbers[j]);
+			if(numbers[i] != 8 || numbers[j] ==9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
