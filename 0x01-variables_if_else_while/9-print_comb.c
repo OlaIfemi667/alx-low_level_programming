e0b8cc3c830b4fdd1989ba98e996e5749b1d0a68
Jean de Dieu BANCOLE <bancolejeandedieu@gmail.com>
2023-09-08 05:40:05 -0700

    less,great,zero

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers[10], i;
+
+	for (i = 0; i < 10; i++)
+	{
+		numbers[i] = i;
+	}
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + numbers[i]);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
