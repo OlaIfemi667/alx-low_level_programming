a93943d1144e613340bdff36877622e8a889a41b
Jean de Dieu BANCOLE <bancolejeandedieu@gmail.com>
2023-09-12 05:32:16 -0700

    good

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * _islower - check if a letter is lowercase
- * c is the int correspondance of the char in ascii table
+ *@c is the int correspondance of the char in ascii table
  * Return: 0 if lowercase and 1 otherwise
  */
-int _islower(int c)//c is the int correspondance of the char in ascii table
+int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 		return (1);
