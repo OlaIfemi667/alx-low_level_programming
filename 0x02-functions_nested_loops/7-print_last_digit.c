0f624cb752cb2833b311cd208ced0d8c885ee2ca
Jean de Dieu BANCOLE <bancolejeandedieu@gmail.com>
2023-09-12 08:21:16 -0700

    good

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,12 @@ int print_last_digit(int a)
 {
 	if (a >= 0)
 	{
-		_putchar('0' + a%10);
-		return (a%10);
+		_putchar('0' + a % 10);
+		return (a % 10);
 	}
 	else
 	{
-		_putchar('0' + (-a)%10);
-		return (-a)%10;
+		_putchar('0' + -a % 10);
+		return (-a) % 10;
 	}
 }
