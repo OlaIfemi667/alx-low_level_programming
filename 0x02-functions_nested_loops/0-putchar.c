780b1913c912b0129ff04ddb18d2c3a888e330aa
Jean de Dieu BANCOLE <bancolejeandedieu@gmail.com>
2023-09-12 03:04:22 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ *
+ */
+int main (void)
+{
+	char *mot = "_putchar";
+	while (mot[i]!= '\0')
+	{
+		putchar(mot[i]);
+	}
+	return (0);
+}
