34ce786524fdf8129cc657edcd0c3a0cc1e771e3
Jean de Dieu BANCOLE <bancolejeandedieu@gmail.com>
2023-09-12 09:17:30 -0700

    good

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h> 
 /**
  * main - check the code
  *
@@ -10,6 +11,7 @@ int main(void)
 
     print_last_digit(98);
     print_last_digit(0);
+    print_last_digit(INT_MIN);
     r = print_last_digit(-1024);
     _putchar('0' + r);
     _putchar('\n');
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <limits.h>
 /**
  * print_last_digit - check the code
  * @a: number
@@ -10,6 +10,10 @@ int print_last_digit(int a)
 	if (a < 0)
 	{
 		a = -a;
+	}
+	if (a == INT_MIN)
+	{
+		a = INT_MAX % 1000 + 1;
 	}
 	_putchar('0' + a % 10);
 	return (a % 10);
