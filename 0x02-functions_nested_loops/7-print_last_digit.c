380c51cb21be8ac8a06e9097ecc33b5c0bade7c8
Jean de Dieu BANCOLE <bancolejeandedieu@gmail.com>
2023-09-12 08:13:37 -0700

    good

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ * @a: number
+ * Return: Always 0.
+ */
+int print_last_digit(int a)
+{
+	if (a >= 0)
+	{
+		_putchar('0' + a%10);
+		return (a%10);
+	}
+	else
+	{
+		_putchar('0' + (-a)%10);
+		return (-a)%10;
+	}
+}
