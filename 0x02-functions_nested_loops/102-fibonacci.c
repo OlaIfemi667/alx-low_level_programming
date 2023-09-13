c190b15bb493a5f9feff469bc84d20562f9c4551
Jean de Dieu BANCOLE <bancolejeandedieu@gmail.com>
2023-09-12 19:39:34 -0700

    good

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,14 +8,14 @@ void generateFibonacci(int n);
 
 void generateFibonacci(int n)
 {
-       	int fib[n];
+       	int fib[n], i;
 	fib[0] = 1;
 	fib[1] = 2;
-	for (int i = 2; i < n; i++)
+	for (i = 2; i < n; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
 	}
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", fib[i]);
 		if (i < n - 1)
