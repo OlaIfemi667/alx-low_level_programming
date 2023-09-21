028e7d8ab6d4c13361846f7db3fff13cd91a3a8c
Jean de Dieu BANCOLE <bancolejeandedieu@gmail.com>
2023-09-21 02:06:24 -0700

    good

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _strcat - check the code
+ * *dest: string 
+ * *src: string
+ * Return: Always 0.
+ */
 char *_strcat(char *dest, char *src)
 {
 	char *final_dest = dest;
