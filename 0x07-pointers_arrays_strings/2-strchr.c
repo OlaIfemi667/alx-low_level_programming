e6ab587ebe2d15af48e91a36797742afadf81cc9
Jean de Dieu BANCOLE <bancolejeandedieu@gmail.com>
2023-09-25 13:20:50 -0700

    /root/push_script.sh

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,12 @@ char *_strchr(char *s, char c)
 	unsigned int i;
 
 	i = 0;
-	while (s[i] != '\0')
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
 		}
-		i++;
 	}
 	return (NULL);
 }
