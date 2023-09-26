c960f938d7a9f98af874199500ea6fcbd4867541
Jean de Dieu BANCOLE <bancolejeandedieu@gmail.com>
2023-09-25 19:19:50 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_chessboard - Affiche un tableau d'échecs (8x8) en console.
+ *
+ * @a: Le tableau d'échecs à afficher.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,5 +3,5 @@
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 int _putchar(char c);
-void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 #endif
