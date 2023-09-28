8906436cde6f3ff8d8a95831e9ce9fa9ef4c0a2e
Jean de Dieu BANCOLE <bancolejeandedieu@gmail.com>
2023-09-28 05:41:24 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _print_rev_recursion - print a string
+ *
+ * @s: the string
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	char *p = '\0';
+
+	p = malloc(sizeof(char ));
+	if (*s != '\0')
+	{
+		p--;
+		*p = *s;
+		s++;
+		_print_rev_recursion(s);
+	}
+	putchar(*p);
+}
