b2e368c9920175f4a13dd243fb00e0bc6881955a
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-02 13:28:03 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: nombre d'argument
+ * @argv: tableau contenant les arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
