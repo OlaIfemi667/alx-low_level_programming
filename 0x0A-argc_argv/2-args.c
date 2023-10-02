40c6abd9c61472252a1e5316505feb028c91344b
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-02 13:42:04 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: nombre d'argument
+ * @argv: tableau
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
