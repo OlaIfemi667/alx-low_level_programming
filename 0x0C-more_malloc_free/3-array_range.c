aba722fc1b1dd991c6e83313dd472a30af0fda16
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-09 14:20:33 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,7 +10,7 @@
 
 int *array_range(int min, int max)
 {
-	int i, j = 0, taille;
+	int i, taille;
 	int *p;
 
 	if (min > max)
@@ -21,10 +21,7 @@ int *array_range(int min, int max)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = min; i <= taille; i++)
-	{
-		p[j] = i;
-		j++;
-	}
+	for (i = 0; min <= max; i++)
+		p[i] = min++;
 	return (p);
 }
