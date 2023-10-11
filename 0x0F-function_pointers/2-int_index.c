e4b5de22d8f5c6049a6a532f0aafda815f4cd204
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-11 11:37:10 -0700

    int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * int_index - find index
+ * @array: le tableau
+ * @size: la taille
+ * @cmp: la fonction
+ */
+ int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	if (array != NULL && cmp != NULL)
+	{
+		if (size <= 0)
+			return (-1);
+
+		for  (i = 0; i < size; i++)
+			if (cmp(array[i]) != 0)
+				return (i);
+	}
+	return (-1);
+}
