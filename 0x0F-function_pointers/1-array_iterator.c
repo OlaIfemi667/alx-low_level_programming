05339b3caf18373ba07b7162efaffb075aa5b208
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-11 11:04:32 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "function_pointers.h"
 
 /**
  * array_iterator - appliquer une fonction au element dun tableau
@@ -8,11 +8,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
-	if (array == NULL || action == NULL)
-		return;
+	if (array != NULL && action != NULL)
+	{
+		unsigned int i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
-	
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
