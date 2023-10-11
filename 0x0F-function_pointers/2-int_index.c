fa5a851b6e9deeede4949a6b720f67e1801be451
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-11 11:41:54 -0700

    int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,10 +6,12 @@
  * @array: le tableau
  * @size: la taille
  * @cmp: la fonction
+ * Return: int
  */
- int int_index(int *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
 	if (array != NULL && cmp != NULL)
 	{
 		if (size <= 0)
