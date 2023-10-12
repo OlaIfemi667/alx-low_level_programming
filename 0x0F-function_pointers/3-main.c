aa9fb205cf361479c679744ff93ccc6e08f3817d
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-11 19:57:27 -0700

    operation

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
+
 /**
  * main - main
  * @argc: nombre argument
@@ -8,14 +9,14 @@
  * Return: 0
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc == 4)
 	{
 		int a = atoi(argv[1]), b = atoi(argv[3]);
 		char *oper = argv[2];
 
-		if (get_op_func(argv[2]) != NULL)
+		if (get_op_func(argv[2]) != NULL && oper[1] == '\0')
 		{
 			if (b == 0 && (*oper == '/' || *oper == '%'))
 			{
