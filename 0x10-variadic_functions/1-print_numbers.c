c0a672aa94804302202fd1c2b53becb3b6865a55
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-12 10:46:01 -0700

    task1_variadic_function.h

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - afficher les nombres
+ * @separator: le separateur
+ * @n: nombre de entier passé en arguments
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+	va_start(ap, n);
+	if (separator == NULL)
+	{
+		for (i = 0 ; i < n; i++)
+			printf("%d", va_arg(ap, int));
+		printf("\n");
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (i == n - 1)
+				printf("%d", va_arg(ap, int));
+			else
+				printf("%d%s", va_arg(ap, int), separator);
+		}
+		printf("\n");
+	}
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,5 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
