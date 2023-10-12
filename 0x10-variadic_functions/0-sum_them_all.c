25cf462248cb43724c252eaa46d33495d0eea419
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-12 10:00:15 -0700

    task0_Variadic_function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <STDIO.H>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - faire la somme des n premiers argumments
+ * @n: le nombre d'argument a spécifié
+ * Return: the sum of these n argument
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, the_sum;
+
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+
+	the_sum = 0;
+
+	for (i = 0; i < n; i++)
+		the_sum = va_arg(ap, int);
+	va_end(ap);
+	return (sum);
+}
