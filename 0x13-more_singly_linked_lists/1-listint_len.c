c8ec7e720f2d3c586588c102ea278dd1a060dfb0
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-24 10:57:22 -0700

    listint_len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * listint_len - afficher la chaine
+ * @h: la structures
+ * Return: le nombre de noeud
+ */
+
+size_t listint_len(const listint_t *h);
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
