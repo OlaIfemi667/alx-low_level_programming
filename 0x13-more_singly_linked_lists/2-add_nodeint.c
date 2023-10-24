e6e647be2017687d5a78e5644e21b31c603ce30a
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-24 11:54:03 -0700

    addnode

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -18,7 +18,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	{
 		new->n = n;
 		new->next = *head;
-		if (new ==NULL)
+		if (new == NULL)
 			return (NULL);
 		*head = new;
 		return (new);
