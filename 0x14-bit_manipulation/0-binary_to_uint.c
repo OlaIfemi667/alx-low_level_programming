ddf28163b70e92bf4120a0ba3de7b1570a065114
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-26 04:15:42 -0700

    binarytoint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 #include <math.h>
 
@@ -11,22 +12,45 @@
 unsigned int is_binary(const char *b)
 {
 	while (*b != '\0')
-		if (b != '1' && b != '0')
+	{
+		if (*b != '1' && *b != '0')
 			return (0);
+		b++;
+	}
 	return (1);
 }
- /**
+/**
+ * _pow_two - puissance 2
+ * @d: pow
+ * Return: result
+ */
+unsigned int _pow_two(unsigned int d)
+{
+	unsigned int n = 1, i;
+
+	for (i = 0; i < d; i++)
+		n *= 2;
+	return (n);
+}
+/**
  * binary_to_uint - binaire en entier
  * @b: la chaine binaire
  * Return: 0 ou convertion
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int converted = 0, i = 0;
+	unsigned int converted = 0, i, j;
 
-	if (b == NULL | is_binary(b) == 0)
+	if ((b == NULL) | (is_binary(b) == 0))
 		return (0);
-	while (b[i] != '\0')
-		converted += unsigned int(b[i]) * pow(2, i);
+	i = strlen(b) - 1;
+	for (j = 0; j < strlen(b); j++)
+	{
+		if (b[i] == '0')
+			converted += 0 * _pow_two(j);
+		if (b[i] == '1')
+			converted += 1 * _pow_two(j);
+		i--;
+	}
 	return (converted);
 }
