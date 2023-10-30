64dc1000691e0516bc78b3bf82a0d47f6e2d0119
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-30 11:54:15 -0700

    read_textfile

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,4 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 ssize_t read_textfile(const char *filename, size_t letters);
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 #endif
