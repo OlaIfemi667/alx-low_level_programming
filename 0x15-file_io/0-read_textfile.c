f63a13b9ee799a5c44eeaa4c2540023cd6976c71
OlaIfemi667 <bancolejeandedieu@gmail.com>
2023-10-30 11:51:16 -0700

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "main.h"
 /**
  * read_textfile - lire le texte d'un fichier
  * @filename: nom du fichier
  * @letters: nombre de lettre afficher
  * Return: 0 ou letters
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fichier;
-	char *to_print;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
-		return (0);
-	to_print = malloc(sizeof(char) * (letters));
-	if (to_print == NULL)
-		return (0);
-	fichier = fopen(filename, "r");
-	if (fichier == NULL)
+	if (!filename)
 		return (0);
 
-	printf("%s", fgets(to_print, letters, fichier));
-	fclose(fichier);
-	return (letters);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
